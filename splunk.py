d89780a6415a49540bb77110a960d42e7350b58b
Abulfaz Ismayilov <ebulfezismayil@gmail.com>
2025-02-15 20:01:24 +0400

    v2

--- splunk.py
+++ splunk.py
@@ -7,10 +7,12 @@ from xml.dom import minidom
 
 baseurl = 'https://192.168.2.10:8089'
 userName = 'admin'
-password = 'password'
-correlation_search_name = 'Threat - blfz_test - Rule'
-new_search_query = '| inputlookup updated_ioc_domains.csv | head 10'
-new_description = 'Salam Aleykum'
+password = 'P@ssword'
+correlation_search_name = 'Threat - blfz_test - Rule' # Correlation Search
+# correlation_search_name = 'blfz_Excessive Failed Login Activity' # Saved Search
+new_search_query = 'index=main | inputlookup updated_ioc_domains.csv | head 10'
+new_description = 'Salam Aleykum teze rule geldi isti istii'
+#cron_schedule ='*/1 * * * *'
 
 # Authenticate with server.
 http = httplib2.Http(disable_ssl_certificate_validation=True)
@@ -24,13 +26,23 @@ sessionKey = minidom.parseString(serverContent).getElementsByTagName('sessionKey
 
 # Correct Correlation Search API Path
 encoded_search_name = urllib.parse.quote(correlation_search_name)
-update_url = f"{baseurl}/servicesNS/nobody/SplunkEnterpriseSecuritySuite/saved/searches/{encoded_search_name}"
+
+
+# Update Correlation Search:
+update_url = f"{baseurl}/servicesNS/nobody/SplunkEnterpriseSecuritySuite/saved/searches/{encoded_search_name}" 
+
+# Saved Search Operations:
+# update_url = f"{baseurl}/services/saved/searches/{encoded_search_name}" # Update Saved Search
+# update_url = f"{baseurl}//servicesNS/admin/search/saved/searches/{encoded_search_name}" # Delete Saved Search
+
+
 
 # Include additional parameters for correlation search
 update_body = urllib.parse.urlencode({
     'search': new_search_query, 
     'description': new_description,
     'action.correlationsearch.enabled': '1',  # Ensure it remains a correlation search
+    #'cron_schedule':cron_schedule
     #'action.correlationsearch.label': 'Threat Detection'  # Optional: Set a label
 })
 
@@ -45,3 +57,7 @@ if response.status == 200:
 else:
     print(f"Error updating correlation search! HTTP Status: {response.status}")
     print(content.decode('utf-8'))
+
+
+
+#Documentation link: https://docs.splunk.com/Documentation/Splunk/8.0.4/RESTREF/RESTsearch
